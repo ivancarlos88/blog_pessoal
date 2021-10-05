2d5de0a859e245db54bc331f7eed8db79a01e784
ivancarlos88 <ivancarlos.ms.21@gmail.com>
2021-10-05 00:17:12 -0300

    exercicio blog pessoal

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,60 +6,68 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "postagem")
 public class Postagem {
-		
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private long id;
-		
-		@NotNull
-		@Size(min = 5, max = 100)
-		private String titulo;
-		
-		@NotNull
-		@Size(min = 10, max = 500)
-		private String texto;
-		
-		@Temporal(TemporalType.TIMESTAMP)
-		private Date data=new java.sql.Date(System.currentTimeMillis());
 
-		public long getId() {
-			return id;
-		}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 
-		public void setId(long id) {
-			this.id = id;
-		}
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String titulo;
 
-		public String getTitulo() {
-			return titulo;
-		}
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String texto;
 
-		public void setTitulo(String titulo) {
-			this.titulo = titulo;
-		}
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
 
-		public String getTexto() {
-			return texto;
-		}
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
-		public void setTexto(String texto) {
-			this.texto = texto;
-		}
+	public long getId() {
+		return id;
+	}
 
-		public Date getData() {
-			return data;
-		}
+	public void setId(long id) {
+		this.id = id;
+	}
 
-		public void setData(Date data) {
-			this.data = data;
-		}
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 }
--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -1,13 +1,13 @@
 package org.generation.blogPessoal.repository;
 
+import java.util.List;
+
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-
-public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
-	
+
 }
